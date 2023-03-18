b6ec549e017b2c523894f81ec94f861a241e7d00
oladsdan <104092480+oladsdan@users.noreply.github.com>
2023-03-18 19:47:57 +0100

    complete sanity backend for my portfolio

---
+++ sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'dd8su4jf',
+    dataset: 'production'
+  }
+})
---
+++ schemas/index.js
@@ -0,0 +1,17 @@
+import testimonials from "./testimonials"
+import contact from "./contact"
+import abouts from "./abouts"
+import skills from "./skills"
+import workExperience from "./workExperience"
+import brands from "./brands"
+import works from "./works"
+
+export const schemaTypes = [
+    testimonials,
+    contact,
+    abouts,
+    skills,
+    workExperience,
+    brands,
+    works
+]
